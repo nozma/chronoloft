df48cd379b05fac625e408e00f92fd57983997b8
nozma <co.panda.fc3s@gmail.com>
2025-02-10 21:37:11 +0900

    .appをビルド可能に

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,24 @@
 import os
 import secrets
+import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 
 def create_app():
-    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
-    frontend_dist = os.path.join(BASE_DIR, 'frontend', 'dist')
+    if hasattr(sys, '_MEIPASS'):
+        base_dir = sys._MEIPASS
+        env_path = os.path.join(base_dir, 'backend', '.env')
+    else:
+        base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+        env_path = os.path.join(base_dir, 'backend', '.env')
+    #BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+    
+    load_dotenv(env_path)
+    frontend_dist = os.path.join(base_dir, 'frontend', 'dist')
 
     app = Flask(
         __name__,
@@ -18,27 +28,23 @@ def create_app():
         template_folder=frontend_dist
     )
     
-    app.instance_path = os.path.join(BASE_DIR, 'backend', 'instance')
+    db_path = os.environ.get("APP_DB_PATH", os.path.join(app.instance_path, 'app.db'))
+    db_path = os.path.expanduser(db_path)
     
     app.config.from_mapping(
         SECRET_KEY=os.environ.get("SECRET_KEY", secrets.token_hex(16)),
-        SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(app.instance_path, 'app.db'),
+        SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.abspath(db_path),
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
-
-    # instance フォルダが存在しない場合は作成する
+    
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs(app.instance_path, exist_ok=True)
     except OSError:
         pass
 
-    # CORSの有効化
     CORS(app)
-
-    # SQLAlchemyの初期化
     db.init_app(app)
 
-    # Blueprintの登録
     from .routes import register_routes
     register_routes(app)
     
