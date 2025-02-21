9360943d1c0e516579d6c0e2879818eeb89288a2
nozma <co.panda.fc3s@gmail.com>
2025-02-22 01:58:35 +0900

    ActiveなActivityだけ記録を開始できる

--- frontend/src/components/ActivityList.jsx
+++ frontend/src/components/ActivityList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
--- frontend/src/components/AddActivityDialog.jsx
+++ frontend/src/components/AddActivityDialog.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
