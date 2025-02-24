3f7fc163f50b2873b80bc4c9493350c97fe1cb73
nozma <co.panda.fc3s@gmail.com>
2025-02-24 20:28:03 +0900

    categoryテーブルの削除

--- backend/app/models.py
+++ backend/app/models.py
@@ -37,7 +37,6 @@ class Activity(db.Model):
         id (int): 自動採番される主キー。
         is_active (bool): 項目が有効(選択可能)であるかどうかを表す真偽値。デフォルトはTrue。
         name (str): アクティビティの名称（例: "勉強", "運動"）。
-        category_id (int): 関連する Category の外部キー。
         unit (enum): アクティビティの記録単位(回数または経過時間)。
         asset_key (str): Discord Developer Portalで設定した画像に対応するアセットキー。
         created_at (datetime): アクティビティ作成日時。デフォルトは現在時刻。
@@ -46,8 +45,6 @@ class Activity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     is_active = db.Column(db.Boolean, default=True, nullable=False)
     name = db.Column(db.String(80), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-    category = db.relationship('Category', back_populates='activities')
     unit = db.Column(db.Enum(ActivityUnitType), nullable=True)
     asset_key = db.Column(db.String(80))
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
@@ -57,28 +54,7 @@ class Activity(db.Model):
 
     def __repr__(self):
         unit_value = self.unit.value if self.unit is not None else None
-        return f"<Activity id={self.id} name={self.name} unit={unit_value} category_id={self.category_id}>"
-
-class Category(db.Model):
-    """
-    カテゴリーを記録するモデル。
-
-    Attributes:
-        id (int): 自動採番される主キー。
-        name (str): カテゴリーの名称(例: "英語", "数学")。
-        group_id: アクティビティグループのid。
-        position (int): UI側での表示順序。
-    """
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    group_id = db.Column(db.Integer, db.ForeignKey('activity_group.id'), nullable=False)
-    group = db.relationship('ActivityGroup', backref='categories')
-    position = db.Column(db.Integer, nullable=False, default=0, server_default='0')
-    
-    activities = db.relationship('Activity', back_populates='category', lazy=True)
-
-    def __repr__(self):
-        return f"<Category id={self.id} name={self.name}>"
+        return f"<Activity id={self.id} name={self.name} unit={unit_value}>"
 
 class Record(db.Model):
     """
