2805250e3c36f59824d623f5a6517e89156f89fb
nozma <co.panda.fc3s@gmail.com>
2025-02-24 19:25:37 +0900

    FilterContextからcategoryを削除

--- frontend/src/contexts/FilterContext.jsx
+++ frontend/src/contexts/FilterContext.jsx
@@ -5,8 +5,6 @@ const FilterContext = createContext();
 export function FilterProvider({ children }) {
   const [filterState, setFilterState] = useState({
     groupFilter: ``,
-    categoryFilter: ``,
-    categoryFilterName: ``,
     activityNameFilter: ``,
   });
 
