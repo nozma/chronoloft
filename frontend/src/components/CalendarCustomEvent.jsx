10c64363be03961275f9958a8c1b71d22a2abefb
nozma <co.panda.fc3s@gmail.com>
2025-02-24 14:51:20 +0900

    全日イベントではツールチップに開始・終了時刻を含めない

--- frontend/src/components/CalendarCustomEvent.jsx
+++ frontend/src/components/CalendarCustomEvent.jsx
@@ -4,15 +4,23 @@ import { DateTime } from 'luxon';
 
 const CustomEventWrapper = ({ event, children }) => {
     // 開始・終了時刻をフォーマット
-    const start = DateTime.fromJSDate(event.start).toFormat("HH:mm");
-    const end = DateTime.fromJSDate(event.end).toFormat("HH:mm");
-    const tooltipContent = (
-        <div>
+    let tooltipContent;
+    if(event.allDay) {
+        tooltipContent =(
             <div><strong>{event.title}</strong></div>
-            <div>開始: {start}</div>
-            <div>終了: {end}</div>
-        </div>
-    );
+        );
+    } else {
+        const start = DateTime.fromJSDate(event.start).toFormat("HH:mm");
+        const end = DateTime.fromJSDate(event.end).toFormat("HH:mm");
+        tooltipContent = (
+            <div>
+                <div><strong>{event.title}</strong></div>
+                <div>開始: {start}</div>
+                <div>終了: {end}</div>
+            </div>
+        );
+    }
+
 
     return (
         <Tooltip
