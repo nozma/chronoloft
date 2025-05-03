c79a72eeab8c57470edb65756f6bfd5edcdaab0e
nozma <co.panda.fc3s@gmail.com>
2025-05-03 21:06:53 +0900

    1.0.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { version } from './package.json'
+import { version } from '../package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
